5a36e41268fb5d9e0a82e9bbecc1b645fe86172c
Ann Mariel Pacada <ann_mariel_pacada@brown.edu>
2019-06-01 23:31:15 -0700

    test new variables

--- pokefile question 4.R
+++ pokefile question 4.R
@@ -1,6 +1,7 @@
 #do any variables predict type?
 
-
+#variable: weight 
+#verdict: no
 types <- levels(pokemon$type1)
 type_weight <- c()
 for (i in types){
@@ -17,5 +18,38 @@ avg_weight <- data.frame(
 )
 ggplot(avg_weight, aes(x = type, y = weight)) + geom_point()
 
+#variable: height
+#verdict: no
+types <- levels(pokemon$type1)
+type_height <- c()
+for (i in types){
+  t <- mean(subset(pokemon, type1 == i)$height_m)
+  type_height <- c(type_height, t)
+}
+rm(i)
+rm(t)
 
-#weight does not seem to
+avg_height <- data.frame(
+  type = types,
+  height = type_height,
+  stringsAsFactors = FALSE
+)
+ggplot(avg_height, aes(x = type, y = height)) + geom_point()
+
+#variable: HP
+#verdict: no
+types <- levels(pokemon$type1)
+type_hp <- c()
+for (i in types){
+  t <- mean(subset(pokemon, type1 == i)$hp)
+  type_hp <- c(type_hp, t)
+}
+rm(i)
+rm(t)
+
+avg_hp <- data.frame(
+  type = types,
+  hp = type_hp,
+  stringsAsFactors = FALSE
+)
+ggplot(avg_hp, aes(x = type, y = hp)) + geom_point()
